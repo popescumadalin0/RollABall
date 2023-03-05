2e37fa7da9862b5d62ff1d31173beb74955e0f0f
DizzYForester <dizzyforester@gmail.com>
2023-03-05 15:11:51 +0200

    finish game

--- RollABallPopescuConstantinMadalin/Assets/Scripts/PlayerController.cs
+++ RollABallPopescuConstantinMadalin/Assets/Scripts/PlayerController.cs
@@ -1,34 +1,65 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
-    public float Speed = 0;
 
-    private Rigidbody rb;
+    public float speed;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
+
     private float movementX;
     private float movementY;
 
-    // Start is called before the first frame update
+    private Rigidbody rb;
+    private int count;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-    }
 
-    void OnMove(InputValue movementValue)
-    {
-        Vector2 movementVector = movementValue.Get<Vector2>();
+        count = 0;
 
-        movementX = movementVector.x;
-        movementY = movementVector.y;
+        SetCountText();
+
+        winTextObject.SetActive(false);
     }
 
     void FixedUpdate()
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
 
-        rb.AddForce(movement * Speed);
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+
+            count = count + 1;
+
+            SetCountText();
+        }
+    }
+
+    void OnMove(InputValue value)
+    {
+        Vector2 v = value.Get<Vector2>();
+
+        movementX = v.x;
+        movementY = v.y;
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+
+        if (count >= 12)
+        {
+            winTextObject.SetActive(true);
+        }
     }
 }
